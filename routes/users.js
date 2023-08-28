cc365369ea8b320e3cf95436589d775606ef4354
lsl <2460497138@qq.com>
2023-08-28 20:53:59 +0800

    用户列表编辑接口实现

--- routes/users.js
+++ routes/users.js
@@ -18,13 +18,13 @@ router.post('/login', async (ctx) => {
     * 2.let res = await User.findOne({ userName, userPwd },{ userId:1,_id:0})  1代表返回，2代表不返回
     * 3.let res = await User.findOne({ userName, userPwd }).select('userId') select里面是返回的数据
     */
-    let res = await User.findOne({ userName, userPwd },'userId userName userEmail state role deptId roleList')
+    let res = await User.findOne({ userName, userPwd }, 'userId userName userEmail state role deptId roleList')
     // 获取到用户数据在res的_doc中
     const data = res._doc
     // 生成token
     const token = jwt.sign({
       data: data
-    },'jason', { expiresIn: '1h' })
+    }, 'jason', { expiresIn: '1h' })
     if (res) {
       data.token = token
       ctx.body = util.success(data)
@@ -46,7 +46,7 @@ router.get('/list', async (ctx) => {
   if (userName) params.userName = userName
   if (state && state != '0') params.state = state
   try {
-    const query =  User.find(params, { _id: 0, userPwd: 0 })
+    const query = User.find(params, { _id: 0, userPwd: 0 })
     const list = await query.skip(skipIndex).limit(page.pageSize)
     const total = await User.countDocuments(params)
     ctx.body = util.success({
@@ -61,14 +61,37 @@ router.get('/list', async (ctx) => {
   }
 })
 // 删除接口
-router.post('/delete',async (ctx) => {
+router.post('/delete', async (ctx) => {
   const { userIds } = ctx.request.body
-  const res = await User.updateMany({ userId: { $in: userIds } }, { state: 2})
-  if(res.modifiedCount){
-    ctx.body = util.success(res,`共删除成功${res.modifiedCount} 条`)
+  const res = await User.updateMany({ userId: { $in: userIds } }, { state: 2 })
+  if (res.modifiedCount) {
+    ctx.body = util.success(res, `共删除成功${res.modifiedCount} 条`)
     return
-  }else{
+  } else {
     ctx.body = util.fail('删除失败')
   }
+})
+//编辑接口
+router.post('/operate', async (ctx) => {
+  const { userId, userName, userEmail, mobile, job, state, roleList, deptId, action } = ctx.request.body
+  if (action == 'add') {
+    if (!userName || !userEmail || !deptId) {
+      ctx.body = util.fail('参数错误', util.CODE.PARAM_ERROR)
+      return
+    }
+  } else {
+    if (!deptId) {
+      ctx.body = util.fail('部门不能为空', util.CODE.PARAM_ERROR)
+      return
+    }
+  }
+  try {
+    const res = await User.findOneAndUpdate({ userId }, { mobile, job, state, roleList, deptId })
+    ctx.body = util.success(res,'更新成功')
+    return
+   } catch (error) { 
+    ctx.body = util.fail(res,'更新失败')
+   }
+  
 })
 module.exports = router
