207d097469a5e31716d5a475973f8c368cd14de5
lsl <2460497138@qq.com>
2023-08-29 20:45:43 +0800

    菜单新增后端实现

--- routes/menus.js
+++ routes/menus.js
@@ -2,16 +2,29 @@
 const router = require('koa-router')()
 // 导入util
 const util = require('../utils/util')
+// 导入menu模型
 const Menu = require('../models/menuSchema')
 
 // 添加路由前缀 '/menu'
 router.prefix('/menu')
+// 菜单列表
+router.get('/list', async (ctx) => {
+  //ctx.request.query 表示从请求上下文中获取查询参数的对象。
+  const { menuName, menuState } = ctx.request.query
+  const params = {}
+  if(menuName) params.menuName = menuName
+  if(menuState) params.menuState = menuState
+  let rootList = await Menu.find(params) || []
+  // const permissionList = getMenuTree(rootList,null,[])
+  ctx.body = util.success(rootList)
+})
+
 
 
 // 处理对 '/operate' 路径的 POST 请求
 router.post('/operate', async (ctx) => {
   // 从请求体中解构获取 _id、action 和 params 属性
-  const {_id, action,...params} = ctx.request.body
+  const { _id, action, ...params } = ctx.request.body
   let res, info
   try {
     // 如果 action 等于 'add'
@@ -35,7 +48,7 @@ router.post('/operate', async (ctx) => {
       // 删除指定 _id 的菜单项
       await Menu.findByIdAndRemove(_id)
       // 删除所有 parentId 包含指定 _id 的菜单项
-      await Menu.deleteMany({parentId:{$all:[_id]}})
+      await Menu.deleteMany({ parentId: { $all: [_id] } })
       // 设置 info 变量为 '删除成功'
       info = '删除成功'
     }
